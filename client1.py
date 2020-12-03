61b1efa43b507629e02d6f841ede87d896f20bb6
Matija Nalis <mnalis-git@voyager.hr>
2020-12-03 05:32:48 +0100

    add alerts

--- client1.py
+++ client1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python2
 import libtorrent as lt
 import time
 import sys
@@ -17,6 +18,13 @@ while (not h.is_seed()):
    print '\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
       (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
       s.num_peers, state_str[s.state]),
+
+   alerts = ses.pop_alerts()
+   for a in alerts:
+      if a.category() & lt.alert.category_t.error_notification:
+         print(a)
+
+
    sys.stdout.flush()
 
    time.sleep(1)
