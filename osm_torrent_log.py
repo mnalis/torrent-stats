d703cce32c1b8dadea5b3b55ab6573517b7e756c
Matija Nalis <mnalis-git@voyager.hr>
2020-12-19 01:02:09 +0100

    moving hashes to separate sqlite table

--- osm_torrent_log.py
+++ osm_torrent_log.py
@@ -29,17 +29,30 @@ sql.execute('INSERT OR IGNORE INTO trackers (announce_url) VALUES (?)', ('udp://
 sql.execute('INSERT OR IGNORE INTO trackers (announce_url) VALUES (?)', ('udp://tracker-udp.gbitt.info:80/announce',))
 sql.execute('INSERT OR IGNORE INTO trackers (announce_url) VALUES (?)', ('udp://tracker.torrent.eu.org:451',))
 
+sql.execute("""
+  CREATE TABLE IF NOT EXISTS hashes (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    hash char(40) NOT NULL,
+    filename varchar(255),
+    scrape integer NOT NULL DEFAULT 1,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (hash) )
+""")
+
+
 sql.execute(""" 
   CREATE TABLE IF NOT EXISTS torrent_stats (
     tracker_id integer NOT NULL,
-    hash char(40) NOT NULL,
-    timestamp int NOT NULL,
-    peers int,
-    seeds int,
-    complete int,
+    hash_id integer NOT NULL,
+    hash char(40) NOT NULL /* old - delete after upgrade */,
+    timestamp integer NOT NULL,
+    peers integer,
+    seeds integer,
+    complete integer,
     created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE(hash, timestamp, tracker_id),
-    FOREIGN KEY (tracker_id) REFERENCES trackers (id) )
+    UNIQUE(hash_id, timestamp, tracker_id),
+    FOREIGN KEY (tracker_id) REFERENCES trackers (id),
+    FOREIGN KEY (hash_id) REFERENCES hashes (id) )
 """)
 
 hashes = [
@@ -57,6 +70,11 @@ hashes = [
         'ffdae9989b9fb625fd352fa179845fef0049ecbc',
 ]
 
+for h in hashes:
+    sql.execute ('INSERT INTO hashes (hash) VALUES (?) ON CONFLICT DO NOTHING', (h,))
+
+sql.execute('UPDATE torrent_stats SET hash_id=(SELECT id from hashes where hashes.hash=torrent_stats.hash)');
+
 # initialize current time here, as we need it to be constant for ALL trackers/hashes in this program run!
 _now = int(time.time())
 
@@ -72,7 +90,9 @@ def do_tracker (tracker_id, announce_url):
         for hash in scr:
             stats = scr[hash]
             print ('  hash=',hash, ' --- stats=',stats)
-            sql.execute ('INSERT INTO torrent_stats (tracker_id, hash, timestamp, peers, seeds, complete) VALUES (?, ?, ?, ?, ?, ?)', (tracker_id, hash, _now, stats['peers'], stats['seeds'], stats['complete']))
+            hash_id, = [h[0] for h in sql.execute ('SELECT id FROM hashes WHERE hash=?', (hash,))]
+            #print ("hashid=", hash_id)
+            sql.execute ('INSERT INTO torrent_stats (tracker_id, hash, hash_id, timestamp, peers, seeds, complete) VALUES (?, ?, ?, ?, ?, ?, ?)', (tracker_id, hash, hash_id, _now, stats['peers'], stats['seeds'], stats['complete']))
     except:
         print ('  --- scraping FAILED, skipping');
     
@@ -82,20 +102,23 @@ def do_all_trackers():
         do_tracker (tracker_id, announce_url)
 
 def add_hash(hash):
-    if hash in hashes:
-        print ('Skipping duplicate hash', hash)
-    else:
-        print ('Adding hash', hash)
-        hashes.append(hash)
+    sql.execute ('INSERT INTO hashes (hash, scrape) VALUES (?, ?) ON CONFLICT DO NOTHING', (hash.lower(), 1))
 
-# find new torrent files
+# find new torrent files from cmdline (if specified) and add them to the list
 for torrent_filename in argv[1:]:
-  info = lt.torrent_info(torrent_filename)
-  add_hash (str(info.info_hash()))
-    
+    info = lt.torrent_info(torrent_filename)
+    add_hash (str(info.info_hash()))
 
+# get a list of all hashes to scrape
+h_sql = connection.cursor()
+hashes = [h[0] for h in h_sql.execute ('SELECT hash FROM hashes WHERE scrape=1')]
+print ("sql Hashes=", hashes);
+
+
+# update status of all trackers (for all infohashes)
 print ('IPv4/mixed:')
 do_all_trackers()
+
 
 # FIXME - IPv6 does not work! check not only connceting, but different datasctructure, and share back via PR.
 #print ('IPv6 only:')
