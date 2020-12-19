83dd105d17881d9fe88a1f7971b7242500712c65
Matija Nalis <mnalis-git@voyager.hr>
2020-12-19 02:10:09 +0100

    helper shell to fetch and index all latest OSM torrents

---
+++ osm_log_all_torrents.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+BASE="https://planet.openstreetmap.org"
+WGET_OPTS="-q -N"
+wget ${WGET_OPTS} ${BASE}/pbf/planet-latest.osm.pbf.torrent
+wget ${WGET_OPTS} ${BASE}/pbf/full-history/history-latest.osm.pbf.torrent
+
+wget ${WGET_OPTS} ${BASE}/planet/planet-latest.osm.bz2.torrent
+wget ${WGET_OPTS} ${BASE}/planet/full-history/history-latest.osm.bz2.torrent
+
+wget ${WGET_OPTS} ${BASE}/planet/changesets-latest.osm.bz2.torrent
+wget ${WGET_OPTS} ${BASE}/planet/discussions-latest.osm.bz2.torrent
+
+./osm_torrent_log.py *.torrent
