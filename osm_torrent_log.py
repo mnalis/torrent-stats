e2b7c80092a9b53f59e6b097e85548093c6e9c69
Matija Nalis <mnalis-git@voyager.hr>
2020-12-19 01:26:37 +0100

    remove old hardcoded hashes

--- osm_torrent_log.py
+++ osm_torrent_log.py
@@ -44,7 +44,6 @@ sql.execute("""
   CREATE TABLE IF NOT EXISTS torrent_stats (
     tracker_id integer NOT NULL,
     hash_id integer NOT NULL,
-    hash char(40) NOT NULL /* old - delete after upgrade */,
     timestamp integer NOT NULL,
     peers integer,
     seeds integer,
@@ -55,26 +54,7 @@ sql.execute("""
     FOREIGN KEY (hash_id) REFERENCES hashes (id) )
 """)
 
-# do not forget "alter table torrent_stats add hash_id integer NOT NULL;" for transition
-hashes = [
-        '049c08a4934c8a2eace7e92a1f3f01f35b045660',
-        '19d3fb366bf63d547de0abf7f783f6271e9ecfe1',
-        '39a642c6bc8cf99e1bff34b8fc6234bee4b1cf39',
-        '3e3e8164b455174132b1d09c411e6d01d8393780',
-        '46bbe95a64c793127068c3c86c62a3a4f6be1c03',
-        '48cd7fe8a7f37866b28ca47d1f14daec67c26f5c',
-        '57b986081cdf8e3a049c8186c1c07e4d0128161c',
-        '750cc99604ab9239bd1723b03c8b0e27be68e554',
-        'b20a4a3c5dcf6c4195474d68616ec19e8299124c',
-        'd3be6148991b579ff40520ef43d6b87249eda2b6',
-        'd84256fcbf807ba6b1257798176df4ceb3056504',
-        'ffdae9989b9fb625fd352fa179845fef0049ecbc',
-]
-
-for h in hashes:
-    sql.execute ('INSERT OR IGNORE INTO hashes (hash) VALUES (?)', (h,))
-
-sql.execute('UPDATE torrent_stats SET hash_id=(SELECT id from hashes where hashes.hash=torrent_stats.hash)');
+hashes = [];
 
 # initialize current time here, as we need it to be constant for ALL trackers/hashes in this program run!
 _now = int(time.time())
